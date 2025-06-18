0ac2134e9c3f4bbdf2abba7fa1c95adb78e62e7e
ChaniBlau <Hand1D6@mbjcomp.org.il>
2025-06-18 17:53:14 +0300

    seed: added initial seed script with organization and users

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,29 +1,42 @@
-import { PrismaClient, Role} from '@prisma/client';
+import { PrismaClient, Role } from '@prisma/client';
+import bcrypt from 'bcryptjs';
 
 const prisma = new PrismaClient();
 
 async function main() {
-  const org = await prisma.organization.create({
+  const hashedPassword = await bcrypt.hash('123456789', 10);
+  const hashedPassword2 = await bcrypt.hash('123456781', 10);
+  const organization = await prisma.organization.create({
     data: {
       name: 'Nvidia',
       users: {
-        create: {
-          name: 'Yohav Bar',
-          email: 'yohav12@gmail.com',
-          password: '12345678',
-          role: Role.ORG_ADMIN
-        }
-      }
-    }
+        create: [
+          {
+            name:'Yohav Gal',
+            email: 'yoyohgg@gmail.com',
+            password: hashedPassword,
+            role: Role.SUPER_ADMIN,
+          },
+          {
+            name: 'Eden Bar',
+            email: 'ede123dd@gmail.com',
+            password: hashedPassword2,
+            role: Role.RECRUITER,
+          },
+        ],
+      },
+    },
+    include: {
+      users: true,
+    },
   });
 
-  console.log('✅ Everything works! Organization created', org.name);
+  console.log('Seeded Organization with users:', organization);
 }
 
 main()
   .catch((e) => {
-    console.error('❌ Connection error:', e);
+    console.error(e);
+    process.exit(1);
   })
-  .finally(() => {
-    prisma.$disconnect();
-  });
+  .finally(() => prisma.$disconnect());
