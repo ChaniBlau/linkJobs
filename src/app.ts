60cdadd597f4366b828531b644a8e46bb6c0d14d
ChaniBlau <Hand1D6@mbjcomp.org.il>
2025-07-13 12:10:56 +0300

    wip: partial changes in app.ts before merge

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import keywordRoutes from './routes/keyword.routes';
 
 dotenv.config();
 const app = express();
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use('/api/keywords', keywordRoutes);
 
 app.get('/health', (req, res) => {
   res.send('Server is running');
